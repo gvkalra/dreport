caab3ee06b071d84d162f5df54d745f891b478c7
Prasanth Kammampati <prasanth.k@samsung.com>
2015-10-26 11:44:06 +0530

    Basic UI application from IDE

---
+++ inc/dreport.h
@@ -0,0 +1,19 @@
+#ifndef __dreport_H__
+#define __dreport_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "dreport"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.samsung.dreport"
+#endif
+
+#endif /* __dreport_H__ */
